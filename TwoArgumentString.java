80ecc184ff88f36cb4e4a006fc52f22d6f20e6b8
Arandolph543 <april.randolph@gmail.com>
2017-10-06 19:27:33 -0400

    Update TwoArgumentString.java

--- TwoArgumentString.java
+++ TwoArgumentString.java
@@ -1,40 +1,17 @@
 /* Author: April Randolph
 Description: Takes two string arguments and returns only the strings contains those character arguments.
 */
-
 import java.lang.*;
 import java.util.*;
 
 public class TwoArgumentString {
-	
-	
-
 
 	public static String[] containsCharacters(String[] arguments, String charA, String charB) {
 		String[] useStrings = new String[arguments.length];
-		int index = 0;
-		int tempIndex = 0;
-		boolean usesGivenCharacters = true;
-		for(int i = 0;i<= arguments.length-1;i++) {
-			CharSequence charS = arguments[i].subSequence(0, arguments[i].length());
-			// System.out.println("Debug: " + String.valueOf(arguments[i]) + " " + String.valueOf(arguments[i].subSequence(0,arguments[i].length())) + "  seq length " + charS.length() );
-			for(int k = 0;k <=charS.length()-1;k++) {
-				if(tempIndex == i) {
-					boolean characterMatchA = String.valueOf(charS.charAt(k)).equals(charA);
-					boolean characterMatchB = String.valueOf(charS.charAt(k)).equals(charB);
-					// System.out.println("Debug: characters " + String.valueOf(charS.charAt(k)) + " character " + characterMatchA + " " + characterMatchB);
-					if(characterMatchA || characterMatchB) {
-						index++;	
-					}else {
-						usesGivenCharacters = false;
-					}						
-				}
+		for(int i = 0;i<arguments.length;i++) {
+			if(arguments[i].contains(charA) && arguments[i].contains(charB)) {
+				useStrings[i] = arguments[i];
 			}
-			if(usesGivenCharacters) {
-				useStrings[tempIndex] = arguments[i];
-			}
-			usesGivenCharacters = true;
-			tempIndex++;
 		}
 		return useStrings;
 	}
