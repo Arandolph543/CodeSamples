63e59348beb91900868c746531867b7800532f2c
Arandolph543 <april.randolph@gmail.com>
2017-10-06 13:50:26 -0400

    Update AdjacentDuplicateRemoval.java

--- AdjacentDuplicateRemoval.java
+++ AdjacentDuplicateRemoval.java
@@ -5,19 +5,22 @@ import java.io.*;
 class AdjacentDuplicateRemoval {
     
     public static String removeAdjacentDups(String string) {
-        char[] arrayChars = string.toCharArray();
-        StringBuilder build = new StringBuilder();
-        for(int i = 0;i<arrayChars.length;i++) {
-            if(!arrayChars[i].equals(arrayChars[i+1]) {
-                build.append(arrayChars[i]);
-            }
-        }
-        return build.toString();
+        StringBuilder sb = new StringBuilder();
+		if (string.length() > 0) {
+			char prev = string.charAt(0);
+			sb.append(prev);
+			for (int i = 1; i < string.length(); ++i) {
+				char cur = string.charAt(i);
+				if (cur != prev) {
+					sb.append(cur);
+					prev = cur;
+				}
+			}
+		}
+        return sb.toString();
     }
     
-    
-    
-	public static void main (String[] args) {
+    public static void main (String[] args) {
 	    AdjacentDuplicateRemoval classAdjacentDuplicateRemoval = new AdjacentDuplicateRemoval();
 	    BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         int numOfInputs = 0;
@@ -29,13 +32,10 @@ class AdjacentDuplicateRemoval {
             substrings = new String[numOfInputs];
             for(int i = 0; i< numOfInputs;i++) {
                 sentences[i] = buffer.readLine(); 
-				//System.out.println(sentences[i]);
-				substrings[i] = classAdjacentDuplicateRemoval.removeAdjacentDups(sentence[i]);
-				
+		substrings[i] = classAdjacentDuplicateRemoval.removeAdjacentDups(sentence[i]);		
             }  
-
-			for(int d = 0;d<numOfInputs;d++) {
-			    System.out.println(substrings[d]);
-			}
+            for(int d = 0;d<numOfInputs;d++) {
+		System.out.println(substrings[d]);
+            }
 	}
 }
