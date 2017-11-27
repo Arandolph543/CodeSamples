5d54b13ae1fb0ec88018843d41116639e8376ec9
Arandolph543 <april.randolph@gmail.com>
2017-11-27 16:46:17 -0500

    Deleted from wrong directory.

--- Pricing/src/main/java/com/pricing/PricingStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.pricing;
-
-import java.math.BigDecimal;
-
-import com.pricing.model.Consumer;
-
-public interface PricingStrategy {
-	public BigDecimal getInsurancePrice(Consumer consumer, BigDecimal price);
-}
