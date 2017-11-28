da35a22e1bebe60463dcce19e6c53d682473e2a9
Arandolph543 <april.randolph@gmail.com>
2017-11-28 10:02:47 -0500

    Merlin Guides image loading detection using callback.

---
+++ JavaScript/ImageLoader.js
@@ -0,0 +1,20 @@
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+    console.log("State: ", this.readyState, "Status: ", this.status);
+    if (this.readyState == 4 && this.status == 200){
+        var url = window.URL || window.webkitURL;
+        document.querySelector("#img").src = 
+        url.createObjectURL(this.response);
+    }else if(this.readyState == 4){
+        console.log(this.status);
+    }else{
+    	var blob = this.response;
+        callback(blob);
+    }
+}
+xhr.open('GET', 'image.png');
+xhr.responseType = 'blob';	
+setTimeout(function(){
+	xhr.send();
+},2000);
