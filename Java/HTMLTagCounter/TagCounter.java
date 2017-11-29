a80ede1122169c84d0f03377138a279e6b92f928
Arandolph543 <april.randolph@gmail.com>
2017-11-29 09:12:30 -0500

    Adding project folder for Merkle TagCounter Project.

---
+++ Java/HTMLTagCounter/TagCounter.java
@@ -0,0 +1,27 @@
+
+public class TagCounter implements Runnable {
+
+	private int count = 0;
+	
+	public TagCounter() {
+	}
+
+	boolean store(String url) {
+	
+	
+	}
+	
+	int retrieve(String url, String tagName) {
+	
+	}
+
+
+	@Override
+	public void run() {
+	
+	
+	
+	}
+
+
+}
