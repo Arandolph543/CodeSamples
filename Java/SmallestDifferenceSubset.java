a31d23fc17070c3c717179baf77afda9f0c29ee7
Arandolph543 <april.randolph@gmail.com>
2018-05-10 19:23:40 -0700

    Adding problems SmallestDifferenceSubset.java and SmallestDifferenceSubsetRecursive.java.

---
+++ Java/SmallestDifferenceSubset.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class SmallestDifferenceSubset {
+
+
+	static int getSmallestSubsetDifference(int[] array) {
+		Stack<Integer> createSubset = new Stack<Integer>();
+		Arrays.sort(array);
+		for(int i = 0;i<array.length;i++) {
+			createSubset.push(array[i]);	
+		}
+		int[] subset1 = new int[array.length/2 +1];
+		int[] subset2 = new int[array.length/2 +1];
+		int index = 0;
+		while(createSubset.empty() == false) {
+			subset1[index] = createSubset.pop();
+			subset2[index] = createSubset.pop();
+			index++;
+		}
+		int sum1 = 0;
+		int sum2 = 0;
+		for(int m =0;m<index;m++) {
+			sum1 += subset1[m];
+			sum2 += subset2[m];
+		}
+		int difference = 0;
+		if(sum1 > sum2) {
+			difference = sum1 - sum2;
+		}else {
+			difference = sum2-sum1;
+		}
+		return difference;
+		
+	}
+
+	public static void main(String[] args) {
+		int[] array = {3, 5, 6, 7, 2, 0};
+		System.out.println("Difference of subset: " + getSmallestSubsetDifference(array));		
+	}
+
+}
