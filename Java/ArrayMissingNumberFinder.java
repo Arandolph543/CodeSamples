78fef5c61a8aba45d6588b96491ebbc8d84a739c
Arandolph543 <april.randolph@gmail.com>
2018-06-08 15:45:26 -0700

    Adding ArrayMissingNumberFinder.java

--- Java/ArrayMissingNumberFinder.java
+++ Java/ArrayMissingNumberFinder.java
@@ -2,16 +2,20 @@ public class ArrayMissingNumberFinder {
 
 	static int findMissingNumber(int[] nums) {
 		int n = nums.length - 1;
-		for(int i = int[0];i<n-1;i++) {
-			if(nums[i] != i) {
-				return nums[i-1] + 1;
+		int i = nums[0];
+		if(n == 0) 
+			return i;
+		
+		for(int j = 1;j<n-1;j++) {
+			if(nums[j] != j) {
+				return j;
 			}
 		}
 		return 0;
 	}
 
 	public static void main(String[] args) {
-		int[] numbers = {1, 2, 3, 4, 5, 7, 8, 9};
+		int[] numbers = {0, 1, 2, 3, 4, 5, 7, 8, 9};
 		System.out.println(findMissingNumber(numbers));
 	}
 
