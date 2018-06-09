e5ad982aa9eb5da81034c91aa23e86ac61fcf7f1
Arandolph543 <april.randolph@gmail.com>
2018-06-09 12:23:29 -0700

    Adding ArrayMajorityElement.java problem.

---
+++ Java/ArrayMajorityElement.java
@@ -0,0 +1,33 @@
+
+public class ArrayMajorityElement {
+
+	static int[] found;
+	
+	static int findMajorityNumber(int[] numbers, int n) {
+		int majority = 0;
+		int m = found.length;
+		for(int i = 0;i<m;i++) {
+			for(int j = 0;j<n;j++) {
+				if(i == numbers[j]) {
+					found[i]++;
+				}
+				if(found[i] > majority) {
+					majority = found[i];
+				}
+			}
+		}
+		return majority;
+	}
+
+	public static void main(String[] args) {
+		ArrayMajorityElement ame = new ArrayMajorityElement();
+		int[] numbers = {3, 3, 4, 2, 3};
+		int length = numbers.length;
+		found = new int[10];
+		for(int i = 0;i<10;i++) {
+			found[i] = 0;
+		}
+		
+		System.out.println(findMajorityNumber(numbers, length));		
+	}
+}
