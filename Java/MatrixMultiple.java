75b29ab2cf45a1325c280643bf32aeb077a22afd
Arandolph543 <april.randolph@gmail.com>
2018-06-14 19:22:13 -0700

    Updated practice MatrixMultiple.java.

--- Java/MatrixMultiple.java
+++ Java/MatrixMultiple.java
@@ -1,15 +1,16 @@
+/*Author: April Randolph
+Description: Finding the smallest parenthesis order to multiple chain integer array as matrices.  (Simply remember to use 0 based.)
+*/
 import java.lang.Integer;
+
 public class MatrixMultiple {
 	protected int[][] m;
-	protected int[][] s;
 	
 	int multipleMatrices(int[] matrices) {
-		
 		int n = matrices.length - 1;
 		int index = 0;
 		int operationValue = 0;
 		m = new int[n][n];
-		s = new int[n][n];
 		
 		for(int j = 1;j < n;j++) {
 			for(int k = 0;k < (n - j);k++) {
@@ -19,7 +20,6 @@ public class MatrixMultiple {
 					operationValue = m[k][l] + m[l+1][index] + matrices[k]*matrices[l+1]*matrices[index+1];
 					if(operationValue < m[k][index]) {
 						m[k][index] = operationValue;
-						s[k][index] = l;
 					}
 				}
 			}
