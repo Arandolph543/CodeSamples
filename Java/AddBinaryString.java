d091fef690a691d7a90139b3b49eb82a9004074a
Arandolph543 <springofapril@yahoo.com>
2019-10-19 16:11:38 -0700

    Code Description Comments Update

--- Java/AddBinaryString.java
+++ Java/AddBinaryString.java
@@ -1,9 +1,13 @@
-import java.lang.*;
+/* Description: Takes two String as binary numbers.  Added the the integers as String.
+Author: April Randolph
+Date: 10/19/2019 */
 
+import java.lang.*;
 
 public class AddBinaryString {
 
 	static String addBinary(String str1, String str2) {
+		//Create the integer value of the each String
 		int numToAdd = Integer.parseInt(str1, 2);
 		int numToAdd2 = Integer.parseInt(str2, 2);
 		String result = String.valueOf(numToAdd + numToAdd2);
