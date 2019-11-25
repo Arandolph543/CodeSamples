892608b73cd7e4de7b0e86dfa631c74453f9f189
Arandolph543 <springofapril@yahoo.com>
2019-11-25 15:23:30 -0800

    Updating convertBinaryToBST with input from console.

--- Java/ConvertTreeIntoBST.java
+++ Java/ConvertTreeIntoBST.java
@@ -3,12 +3,15 @@ Author: April Randolph
 Date: 05/28/2018, 10/21/2019 */
 
 import java.lang.*;
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
+
 import java.util.List;
 import java.util.Stack;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.ArrayList;
 
 class Node {
 
@@ -31,8 +34,8 @@ class ConvertTreeIntoBST {
 		dfs(tree.left);
 		list.add(tree.data);
 		dfs(tree.right);	
-	
 	}
+	
 	void contructTree(Node root) {
 		if(root == null) 
 			return;
@@ -47,7 +50,7 @@ class ConvertTreeIntoBST {
 		dfs(root);
 		Collections.sort(list);
 		Collections.reverse(list);
-		list.forEach(System.out::println);
+		list.forEach(System.out::print);
 		stack.addAll(list);
 		
 		contructTree(root);
@@ -55,13 +58,28 @@ class ConvertTreeIntoBST {
 	}
 	
 	public static void main(String[] args) {
-		ConvertTreeIntoBST tree = new ConvertTreeIntoBST();
-		tree.root = new Node(4);
-		tree.root.left = new Node(6);
-		tree.root.right = new Node(2);
-		tree.root.left.left = new Node(5);
-		tree.root.right.left = new Node(4);
-		tree.root.right.left.left = new Node(1);
-		Node binarySearchTree = tree.convertBinaryToBST(tree.root);
+		ConvertTreeIntoBST treeCTIBclass = new ConvertTreeIntoBST();
+		System.out.println("Please enter the numbers from pre-order input of Binary tree (six is the tree size): ");
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+		List<String> listIntegers = new ArrayList<>();
+		String[] array = null;
+		try {
+			String input = buffer.readLine();
+			if(input!=null) {
+				array = input.split(" ");
+				for(String item: array) {
+					listIntegers.add(item);
+				}
+			}
+			treeCTIBclass.root = new Node(new Integer(listIntegers.get(0)));
+			treeCTIBclass.root.left = new Node(Integer.parseInt(listIntegers.get(1))); 
+			treeCTIBclass.root.right = new Node(Integer.parseInt(listIntegers.get(2)));
+			treeCTIBclass.root.left.left = new Node(Integer.parseInt(listIntegers.get(3)));
+			treeCTIBclass.root.right.left = new Node(Integer.parseInt(listIntegers.get(4)));
+			treeCTIBclass.root.right.left.left = new Node(Integer.parseInt(listIntegers.get(5)));
+		} catch(IOException ex) {
+			System.out.println(ex);
+		}
+  		Node binarySearchTree = treeCTIBclass.convertBinaryToBST(treeCTIBclass.root);
 	}
 }
