16b495ff075f3510418b354ec3936e7db7fbd42e
Arandolph543 <springofapril@yahoo.com>
2020-03-05 10:57:29 -0800

    Added practice problem adding comments.

--- Java/OneEdit.java
+++ Java/OneEdit.java
@@ -1,3 +1,8 @@
+/*Description: Find the string that needs to not be edited given two Strings.
+Author: April Randolph
+Date: 03/05/20*/
+
+import java.io.*;
 public class OneEdit {
 
 	int min(int x, int y, int z) {
@@ -31,7 +36,16 @@ public class OneEdit {
 		OneEdit oe = new OneEdit();
 		String strEditable1 = "saturday";
 		String strEditable2 = "Sunday";
-		System.out.println("The string saturday and sunday are : " + oe.checkOneEdited("saturday", "sundays", strEditable1.length(), strEditable2.length()));
+		String strEditable = "sat";
+		try {
+			BufferedReader inputStringReader = new BufferedReader(new InputStreamReader(System.in));
+			strEditable1 = inputStringReader.readLine();
+			strEditable2 = inputStringReader.readLine();
+			System.out.println("The string " + strEditable1 + " " + strEditable2 + "  : " + oe.checkOneEdited(strEditable1, strEditable2, strEditable1.length(), strEditable2.length()));
+			
+		} catch(IOException ex){
+			System.out.println(ex);
+		}
 	}
 
 }
