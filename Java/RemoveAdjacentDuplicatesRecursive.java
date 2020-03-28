7337917ba194e042779aa4ee7e52cf4e2c33c8e3
Arandolph543 <springofapril@yahoo.com>
2020-03-28 11:52:08 -0700

    Update to interview practice problem user input.

--- Java/RemoveAdjacentDuplicatesRecursive.java
+++ Java/RemoveAdjacentDuplicatesRecursive.java
@@ -1,12 +1,16 @@
+/*Description: Recursively remove adjacent duplicates. 
+Author:  April Randolph
+Date: 03/27/20
+*/
+import java.io.*;
+
 public class RemoveAdjacentDuplicatesRecursive {
 
 	static String removeAdjDuplicates(String phrase, int index, int n, String res) {
 		if(index>=n) {
-			if(n>=2&&(phrase.charAt(n-1)!=phrase.charAt(n-2))) {
+			if(n>=2&&(phrase.charAt(n-1)!=phrase.charAt(n-2))|| n==1) {
 				res += phrase.charAt(n-1);
-			} else if(n==1) {
-				res += phrase.charAt(n-1);
-			}
+			} 
 			return res;
 		}
 		char ch = phrase.charAt(index);
@@ -22,14 +26,18 @@ public class RemoveAdjacentDuplicatesRecursive {
 	}
 
 	public static void main(String[] args) {
-		String string = "messymisssolessomemessss";
-		int n = string.length();
-		char[] arr1 = removeAdjDuplicates(string, 0, n, "").toCharArray();
-		char[] arr2 = removeAdjDuplicates(String.valueOf(arr1), 0, arr1.length, "").toCharArray();
-		while(arr1.length!= arr2.length) {
-			arr1 = removeAdjDuplicates(String.valueOf(arr2), 0, arr2.length,"").toCharArray();
-			arr2 = removeAdjDuplicates(String.valueOf(arr1), 0, arr1.length,"").toCharArray();
-		}
-		System.out.println(String.valueOf(arr1));	
+		try {
+			BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Please enter a number of strings enter then the string to remove the adjacent duplicates:");
+			int count = Integer.parseInt(buffer.readLine());
+			while(count>0) {
+				String input = buffer.readLine();
+				String result = removeDuplicatesAdj(input, "", 0, input.length());
+				count--;
+				System.out.println("The new deduplicated adjacent string character string is: " + result + " for " + input);
+			}
+		} catch(IOException ex) {
+			System.out.println(ex);
+		}		
 	}
 }
