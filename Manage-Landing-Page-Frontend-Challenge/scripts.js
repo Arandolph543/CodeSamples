7253b3bd634613e1cbf21a3a4a0cbffaa7c72fb2
Arandolph543 <springofapril@yahoo.com>
2020-07-14 16:11:14 -0700

    Workspace deletion of unnecessary files.

--- Manage-Landing-Page-Frontend-Challenge/scripts.js
+++ Manage-Landing-Page-Frontend-Challenge/scripts.js
@@ -1,21 +1,6 @@
 /* Description: These are added animations that not in the requirements though as the latest frontend styles are available the implementation is be added for up to date code implementation. 
 Author: April Randolph Date: 07/13/20 */
 
-/* 	  
-@keyframes flyin {
-   to { 
-     -webkit-filter: blur(0);
-     transform: scale(1);
-     opacity: 1;
-   }
-   0%   { 
-   transform: translateX(-20%); 		
-   }
-   100% { 
-   transform: translateX(0%); 
-   }
-}
- */
 $(function() {
     var boxDraw = $(".sectionrow");
     var boxDrawTop = boxDraw.offset().top;
