dea3ed52d69ce062880588fc8e7306533ab96786
Arandolph543 <springofapril@yahoo.com>
2020-07-14 15:51:45 -0700

    Added animation.

--- Manage-Landing-Page-Frontend-Challenge/scripts.js
+++ Manage-Landing-Page-Frontend-Challenge/scripts.js
@@ -1,18 +1,31 @@
 /* Description: These are added animations that not in the requirements though as the latest frontend styles are available the implementation is be added for up to date code implementation. 
 Author: April Randolph Date: 07/13/20 */
 
-function myMove() {
-	  var elem = document.getElementById("sectionfirst");   
-	  var pos = -230;
-	  var id = setInterval(frame, 10);
-	  function frame() {
-		if (pos == 350) {
-		  clearInterval(id);
-		} else {
-		  pos++; 
-		  elem.style.right = pos + 'px'; 
-		  elem.style.left = pos + 'px'; 
-		}
-	}
+/* 	  
+@keyframes flyin {
+   to { 
+     -webkit-filter: blur(0);
+     transform: scale(1);
+     opacity: 1;
+   }
+   0%   { 
+   transform: translateX(-20%); 		
+   }
+   100% { 
+   transform: translateX(0%); 
+   }
 }
-	  
+ */
+$(function() {
+    var boxDraw = $(".sectionrow");
+    var boxDrawTop = boxDraw.offset().top;
+    var windowHeight = $(window).height();
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if ( scroll >= ( boxDrawTop - windowHeight )) {
+            boxDraw.addClass("draw");
+        } 
+    });
+});
+ 
