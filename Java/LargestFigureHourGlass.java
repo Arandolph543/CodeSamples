5a655638a8818086a95c3ff390685bcfa89b1c00
Arandolph543 <springofapril@yahoo.com>
2020-08-22 17:22:10 -0700

    Update LargestFigureHourGlass.java
    Added problem statement with description.

--- Java/LargestFigureHourGlass.java
+++ Java/LargestFigureHourGlass.java
@@ -1,3 +1,6 @@
+/* Description: Given matrix of integers find the largest hourglass returning the integer count.
+Author: April Randolph
+Date:08/22/20 */
 public class LargestFigureHourGlass {
 
 	static int largestHourGlass(int[][] arr) {
@@ -8,7 +11,6 @@ public class LargestFigureHourGlass {
 			for(int h = 0;h<m;h++) {
 				if(h+2 <m && i+2 < n && arr[i][h] > 0 && arr[i][h+2] > 0) {
 					int i1 = i,i2 = i+1, m1 = h, m2 = h+2;
-					System.out.println("Debug: " + arr[i][h] + " " + m2 + " " + i+2);
 					while(i1<=i2+1 && m1<=m2) {
 						if(!(i1 == i2 && h == m1) && !(i1 == i2 && h == m2)) {
 							tempSum += arr[i1][m1];
