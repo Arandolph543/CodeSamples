7d97eac7c93b67fddba5c150f8f302e0bc7be1d6
Arandolph543 <springofapril@yahoo.com>
2021-03-10 08:31:20 -0800

    Update SubArrayLargestOfK.java

--- Java/SubArrayLargestOfK.java
+++ Java/SubArrayLargestOfK.java
@@ -1,30 +1,31 @@
+/*Description: Finding the subarray with the largest sum of the size k. 
+Author: April Randolph
+Date: 03/10/21*/
+
 import java.util.*;
 public class SubArrayLargestOfK {
 
 	static int findLargestSubarray(int[] numbers, int k) {
-		int sum =0;
-		int maximum = 0;
+		int sum, maximum, curr_max =0;
 		int[] maxSum = new int[numbers.length];
 		maxSum[0] = numbers[0];
 		for(int i = 0;i<numbers.length;i++) {
 			maximum = Math.max(sum, maximum+numbers[i]);
 			maxSum[i] = maximum;
 		}
-		System.out.println("sum: " + sum);
+		
 		for(int j = 0;j<k;j++) {	
 			sum += numbers[j];
 		}	
-		System.out.println("K Numbers: " + sum);
-		
+
 		int curr_max = 0;
 		int tempNum = sum;
 		for(int l = k;l<numbers.length;l++) {
 			sum = sum + numbers[l] - numbers[l-k];
 			
 			System.out.println("Maximum : " + tempNum);
-			maximum = Math.max(tempNum, sum);
-			maximum = Math.max(maximum, sum + maxSum[l-k]);
-		System.out.println("Debug:  " + curr_max + "  " + maximum + "  " + sum);
+			curr_max = Math.max(tempNum, sum);
+			maximum = Math.max(Math.max(curr_max,  maximum), sum + maxSum[l-k]));
 		}
 		return maximum;
 	
