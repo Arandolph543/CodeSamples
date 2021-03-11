f34322ef4e6555ded2d28bf5d84b01797742f2a8
Arandolph543 <springofapril@yahoo.com>
2021-03-10 20:14:38 -0800

    Update FindNextLargest.java

--- Java/FindNextLargest.java
+++ Java/FindNextLargest.java
@@ -11,10 +11,10 @@ public class FindNextLargest {
 		}
 		for(int i=0;i<n;i++) {
 		    for(int j = 0;j<n;j++) {			
-				if(array[i] < array[j] && j > i) {
-					nextLargest[i] = array[j];
-					break;
-				} 
+			if(array[i] < array[j] && j > i) {
+				nextLargest[i] = array[j];
+				break;
+			} 
 		    }
 		}
 		return nextLargest;
