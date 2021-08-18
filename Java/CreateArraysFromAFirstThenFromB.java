a5aeb90c2e3b36eb5e70b85d57373691d37f3c4e
Arandolph543 <springofapril@yahoo.com>
2021-08-18 14:22:36 -0700

    Update CreateArraysFromAFirstThenFromB.java
    Added Description for problem statement. Cleaned up the code formatting for better readability.

--- Java/CreateArraysFromAFirstThenFromB.java
+++ Java/CreateArraysFromAFirstThenFromB.java
@@ -1,3 +1,7 @@
+/* Author: April Randolph
+Description: Create all sorted arrays first from the given array A then from another array B.
+Date: 08/18/2021*/
+
 import java.lang.*;
 
 public class CreateArraysFromAFirstThenFromB {
@@ -6,7 +10,7 @@ public class CreateArraysFromAFirstThenFromB {
 		for(int i = 0;i<length;i++) {
 			System.out.print(C[i] + " ");	
 		}		
-			System.out.println("");			
+		System.out.println("");			
 	}
 
 	void generateUtil(int[] A, int[] B, int[] C, int i, int j, int m, int n, int len, boolean flag) {
@@ -17,7 +21,6 @@ public class CreateArraysFromAFirstThenFromB {
 			for(int k = i;k<m;k++) {
 				if(len == 0) {
 					C[len] = A[k];
-					
 					generateUtil(A, B, C, k+1, j, m, n, len, !flag);					
 				} else if(A[k] > C[len]) {
 					C[len++] = A[k];
@@ -35,12 +38,10 @@ public class CreateArraysFromAFirstThenFromB {
 	}
 	
 	void generate(int[] A, int[] B, int m, int n) {
-		int[] C = new int[m+n];
-		
+	        int[] C = new int[m+n];
 		generateUtil(A, B, C, 0,0,m,n,0,true);		
 	}
 	
-
 	public static void main(String[] args) {
 		int[] A = {8, 9, 12, 17, 39};
 		int[] B = {3, 7, 11, 45, 67};
@@ -48,7 +49,5 @@ public class CreateArraysFromAFirstThenFromB {
 		int n = B.length;
 		CreateArraysFromAFirstThenFromB generateArrays = new CreateArraysFromAFirstThenFromB();
 		generateArrays.generate(A,B, m,n);
-	
 	}
-
 }
