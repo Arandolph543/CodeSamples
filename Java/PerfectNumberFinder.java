8b727c9aea69e688278b19101171cbb0a7c2b490
April Randolph <springofapril@yahoo.com>
2022-01-05 08:21:59 -0800

    Update PerfectNumberFinder.java
    Added problem description statement and comments.

--- Java/PerfectNumberFinder.java
+++ Java/PerfectNumberFinder.java
@@ -1,16 +1,20 @@
+/* Description: The sum of the given integers positive divisors is determined with a string returned for if the number is a perfect number or isn't.
+Author: April Randolph
+Date: 01/05/22*/
+
 import java.lang.*;
 
 public class PerfectNumberFinder {
 
 	public void isPerfectNumber(int numb) {
 		int perNum = 0;
-		
+		//Find and sum the positive divisors
 		for(int i = 1;i<=numb/2;i++) {
 			if(numb%i == 0) {
 				perNum += i;
 			}		
 		}
-				
+		//Check for perfect divisible sum to the input value meets condition		
 		if(perNum==numb) {
 			System.out.println("Your number is perfect number.");
 		} else {
